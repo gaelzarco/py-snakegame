64c69345ff5e39b553eeca2dae29035ededa692d
gael <76548719+gaelzarco@users.noreply.github.com>
2023-02-21 16:23:09 -0800

    created my first pythong turtle graphcis animation

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ import turtle
 
 WIDTH = 500
 HEIGHT = 500
+DELAY = 20
+
+def move_turtle():
+    my_turtle.forward(1)
+    my_turtle.right(1)
+    screen.update()
+    screen.ontimer(move_turtle, DELAY)
 
 screen = turtle.Screen()
 screen.setup(WIDTH, HEIGHT)
 screen.title("Program Title")
 screen.bgcolor("black")
+screen.tracer(0)
 
 my_turtle = turtle.Turtle()
 my_turtle.shape('turtle')
 my_turtle.color('white')
 
-my_turtle.forward(100)
+move_turtle()
 
 turtle.done()
